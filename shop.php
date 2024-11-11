942104c6a4c465195e6a1958d8fb2d7cd163a107
nguyennamanh2202 <115741480+nguyennamanh2202@users.noreply.github.com>
2024-11-11 13:08:31 +0700

    [HTML] - Client - làm chức năng category

--- shop.php
+++ shop.php
@@ -1,6 +1,6 @@
 <?php 
 include 'header.php';
-include 'shop-xuly.php'; // Include file để lấy danh sách sản phẩm từ cơ sở dữ liệu
+include 'shop-xuly.php'; // Include file để lấy danh sách sản phẩm và danh mục từ cơ sở dữ liệu
 ?>
 
 <!-- Navbar End -->
@@ -21,22 +21,44 @@ include 'shop-xuly.php'; // Include file để lấy danh sách sản phẩm t�
 
 <!-- Shop Start -->
 <div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <div class="row pb-3 d-flex justify-content-center">
+    <div class="row">
+        <!-- Sidebar Start -->
+        <div class="col-lg-3 col-md-4">
+            <h5 class="categories-title mb-3">Categories</h5>
+            <div class="bg-light p-4 mb-30">
+                <ul class="list-unstyled category-list mb-0">
+                    <li class="mb-2">
+                        <a href="shop.php" class="text-dark <?php if (!isset($category_id)) echo 'active'; ?>">All Products</a>
+                    </li>
+                    <?php foreach ($categories as $category): ?>
+                        <li class="mb-2">
+                            <a href="shop.php?category_id=<?php echo $category['id']; ?>" 
+                               class="<?php echo (isset($category_id) && $category_id == $category['id']) ? 'active' : ''; ?>">
+                                <?php echo htmlspecialchars($category['category_name']); ?>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+        <!-- Sidebar End -->
+
+        <!-- Product List Start -->
+        <div class="col-lg-9 col-md-8">
+            <div class="row pb-3">
                 <?php if (isset($products) && count($products) > 0): ?>
                     <?php foreach ($products as $product): ?>
-                        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
                             <div class="product-item bg-light mb-4">
                                 <div class="product-img position-relative overflow-hidden">
                                     <img class="img-fluid w-100" 
                                          src="<?php echo (isset($product['image_path']) && !empty($product['image_path'])) ? htmlspecialchars($product['image_path']) : 'img/default.jpg'; ?>" 
                                          alt="<?php echo isset($product['product_name']) ? htmlspecialchars($product['product_name']) : 'Product Image'; ?>">
                                     <div class="product-action">
-                                        <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
-                                        <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
-                                        <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
-                                        <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-shopping-cart"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href="#"><i class="far fa-heart"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-sync-alt"></i></a>
+                                        <a class="btn btn-outline-dark btn-square" href="#"><i class="fa fa-search"></i></a>
                                     </div>
                                 </div>
                                 <div class="text-center py-4">
@@ -73,19 +95,19 @@ include 'shop-xuly.php'; // Include file để lấy danh sách sản phẩm t�
                         <ul class="pagination justify-content-center">
                             <?php if ($current_page > 1): ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="shop.php?page=<?php echo $current_page - 1; ?>">Previous</a>
+                                    <a class="page-link" href="shop.php?category_id=<?php echo $category_id; ?>&page=<?php echo $current_page - 1; ?>">Previous</a>
                                 </li>
                             <?php endif; ?>
 
                             <?php for ($i = 1; $i <= $total_pages; $i++): ?>
                                 <li class="page-item <?php if ($i == $current_page) echo 'active'; ?>">
-                                    <a class="page-link" href="shop.php?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                    <a class="page-link" href="shop.php?category_id=<?php echo $category_id; ?>&page=<?php echo $i; ?>"><?php echo $i; ?></a>
                                 </li>
                             <?php endfor; ?>
 
                             <?php if ($current_page < $total_pages): ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="shop.php?page=<?php echo $current_page + 1; ?>">Next</a>
+                                    <a class="page-link" href="shop.php?category_id=<?php echo $category_id; ?>&page=<?php echo $current_page + 1; ?>">Next</a>
                                 </li>
                             <?php endif; ?>
                         </ul>
@@ -99,4 +121,36 @@ include 'shop-xuly.php'; // Include file để lấy danh sách sản phẩm t�
 <!-- Shop End -->
 
 <!-- Footer Start -->
-<?php include 'footer.php'; ?>
+<?php include 'footer.php'; ?> 
+
+<!-- Custom CSS for Sidebar -->
+<style>
+    /* CSS tùy chỉnh cho phần Categories */
+    .categories-title {
+        font-weight: 700;
+        color: #333;
+        text-transform: uppercase;
+        padding-bottom: 10px;
+        border-bottom: 2px solid #ffc107;
+    }
+
+    .category-list a {
+        display: block;
+        color: #555;
+        padding: 10px 15px;
+        text-decoration: none;
+        font-size: 16px;
+        transition: background 0.3s, color 0.3s;
+        border-radius: 5px;
+    }
+
+    .category-list a:hover {
+        background-color: #ffc107;
+        color: #fff;
+    }
+
+    .category-list a.active {
+        background-color: #ffc107;
+        color: #fff;
+    }
+</style>
